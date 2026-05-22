4815ba44e663420efeff9e7fd98798c1c2fae09c
mekos <me.setiyadi27@gmail.com>
2026-05-22 20:38:07 +0700

    feat: input validation, math function, dan main loop

--- index.js
+++ index.js
@@ -1,1 +1,123 @@
 // code here, goodluck!!
+'use strict';
+
+const prompt = require('prompt-sync')();
+
+function ambilAngka(pesanPrompt) {
+  while (true) {
+  let input = prompt(pesanPrompt);
+
+  if (input.trim() === '') {
+    console.log("Input kosong, Ketik Angkanya dulu dong!\n");
+    continue;
+    }
+
+    let angka = Number(input);
+
+    if (isNaN(angka)) {
+      console.log("Gess, itu bukan angka. Coba lagi!\n");
+    } else {
+      return angka;
+    }
+  }
+}
+
+function ambilOperator() {
+  const daftarValid= ['+', '-', '*', '/', '%', '**'];
+
+  while (true) {
+    let input = prompt("Pilih operator (+, -, *, /, %, **): ");
+
+    if (daftarValid.includes(input)) {
+      return input;
+    } else {
+      console.log("Gess, itu bukan operator. Coba lagi!\n");
+    }
+  }
+}
+
+// ==========================================
+// KUMPULAN FUNCTION MATEMATIKA
+// ==========================================
+
+function tambah(a, b) {
+  return a + b;
+}
+
+function kurang(a, b) {
+  return a - b;
+}
+
+function kali(a, b) {
+  return a * b;
+}
+
+function bagi(a, b) {
+  // Fitur Penting : Handle edge case pembagian dengan nol
+  if (b === 0) {
+    return "Error: Gak bisa dong dibagi dengan nol, Emang Mau bikin black hole?";
+  }
+  return a / b;
+}
+
+function modulo(a, b) {
+  // Fitur Penting : Handle edge case nol dibagi dengan angka
+  if (b === 0) {
+    return "Error: Gak bisa dong dibagi dengan nol, Ganti dulu angka 0 nya gess!";
+  }
+  return a % b;
+}
+
+function pangkat(a, b) {
+  return a ** b;
+}
+
+// ==========================================
+// MAIN LOOP KALKULATOR
+// ==========================================
+
+function mulaiKalkulator() {
+  console.log("\n==========================================");
+  console.log(" KALKULATOR BY MES ");
+  console.log("==========================================\n");
+
+  while (true) {
+    console.log("--- Mulai Perhitungan ---");
+    
+    let angka1 = ambilAngka("Masukkan angka pertama: ");
+    let operator = ambilOperator();
+    let angka2 = ambilAngka("Masukkan angka kedua: ");
+
+    let hasil;
+
+
+    switch (operator) {
+      case '+':
+        hasil = tambah(angka1, angka2);
+        break;
+      case '-':
+        hasil = kurang(angka1, angka2);
+        break;
+      case '*':
+        hasil = kali(angka1, angka2);
+        break;
+      case '/':
+        hasil = bagi(angka1, angka2);
+        break;
+      case '%':
+        hasil = modulo(angka1, angka2);
+        break;
+      case '**':
+        hasil = pangkat(angka1, angka2);
+        break;
+    }
+
+    console.log(`\n=> HASIL: ${angka1} ${operator} ${angka2} = ${hasil}`);
+    
+
+    break; 
+  }
+}
+
+
+mulaiKalkulator();
